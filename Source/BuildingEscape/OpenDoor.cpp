0c129a930d8c7d752221f3e5a4f6e0d504231333
LRBeaver <GitPass16!>
2017-05-31 14:31:43 -0400

    no message

---
+++ Source/BuildingEscape/OpenDoor.cpp
@@ -0,0 +1,42 @@
+// Copyright Lyndsay Beaver 2017
+
+#include "BuildingEscape.h"
+#include "OpenDoor.h"
+
+
+// Sets default values for this component's properties
+UOpenDoor::UOpenDoor()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	bWantsBeginPlay = true;
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UOpenDoor::BeginPlay()
+{
+	Super::BeginPlay();
+	// Find the owning actor
+	AActor* Owner = GetOwner();
+	// Create a rotator
+	FRotator NewRotation = FRotator(0.0f, 90.0f, 0.0f);
+	// Set the door rotation
+	Owner->SetActorRotation(NewRotation);
+	
+	//UE_LOG(LogTemp, Warning, TEXT("Position report for %s!"), *ObjectName);
+	
+}
+
+
+// Called every frame
+void UOpenDoor::TickComponent( float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction )
+{
+	Super::TickComponent( DeltaTime, TickType, ThisTickFunction );
+
+	// ...
+}
+
